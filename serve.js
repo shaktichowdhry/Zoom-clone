4d2565cb3b25bd024dfbf7320d4695026e8d8310
shakti <shaktichowdhry28@gmail.com>
2020-09-03 01:50:29 +0530

    Initialize our first view

--- serve.js
+++ serve.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const server = require('http').Server(app);
 
-// This is where my app lives
+// we dont have to import ejs but tell your app that, view engine is going to be ejs
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.status(200).send("Hello World");
+    // res.status(200).send("Hello World");
+    res.render('room');
 })
 
-// server is local host and port is 3030
-server.listen(3030);
 
-// Now run nodemon serve.js and in chrome go to localhost:3030
+server.listen(3030);
